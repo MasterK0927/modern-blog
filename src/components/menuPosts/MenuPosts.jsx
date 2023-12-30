bfb5ab7b20e27ec0cec0c9eaa310cb91318ab641
Keshav Agarwal <64908981+MasterK0927@users.noreply.github.com>
2023-12-30 18:12:54 +0530

    Update MenuPosts.jsx

--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 import React from "react";
 import styles from "./menuPosts.module.css"
 
+const imageLoader = () => {
+  return `https://lh3.googleusercontent.com/drive-viewer/AEYmBYQuXCbmGMePiC1IjnSLOYaoSPHVJMYvX-klefC2gcTo-sSaJ9m-zXpbuQtN0zydSL2LDjPmpMPSLtELcan1IDHOAJVeCQ=s1600`
+}
+
 const MenuPosts = ({ withImage }) => {
   return (
     <div className={styles.items}>
       <Link href="/" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
-            <Image src="/maskerk.png" alt="" fill className={styles.image} />
+            <Image src={imageLoader} alt="" fill className={styles.image} />
           </div>
         )}
         <div className={styles.textContainer}>
@@ -26,7 +30,7 @@ const MenuPosts = ({ withImage }) => {
       <Link href="/" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
-            <Image src="/maskerk.png" alt="" fill className={styles.image} />
+            <Image src={imageLoader} alt="" fill className={styles.image} />
           </div>
         )}
         <div className={styles.textContainer}>
@@ -45,7 +49,7 @@ const MenuPosts = ({ withImage }) => {
       <Link href="/" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
-            <Image src="/maskerk.png" alt="" fill className={styles.image} />
+            <Image src={imageLoader} alt="" fill className={styles.image} />
           </div>
         )}
         <div className={styles.textContainer}>
@@ -62,7 +66,7 @@ const MenuPosts = ({ withImage }) => {
       <Link href="/" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
-            <Image src="/maskerk.png" alt="" fill className={styles.image} />
+            <Image src={imageLoader} alt="" fill className={styles.image} />
           </div>
         )}
         <div className={styles.textContainer}>
