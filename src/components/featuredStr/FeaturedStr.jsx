3e3d085c4f0a0d9a79f30132d26ccf11168b08d9
Keshav Agarwal <64908981+MasterK0927@users.noreply.github.com>
2024-01-01 21:23:14 +0530

    Update FeaturedStr.jsx

--- src/components/featuredStr/FeaturedStr.jsx
+++ src/components/featuredStr/FeaturedStr.jsx
@@ -16,7 +16,7 @@ const fetcher = async (url) => {
 
 const FeaturedStr = ({ cat }) => {
   const { data, error, isValidating } = useSWR(
-    `https://keshavwrites.netlify.app/api/posts?page=1&cat=${cat || ""}`,
+    `https://keshavwrites.netlify.app/api/posts?page=2&cat=${cat || ""}`,
     fetcher
   );
 
@@ -33,7 +33,7 @@ const FeaturedStr = ({ cat }) => {
       <div>
         {isValidating && <p>Loading...</p>}
         {data && Array.isArray(data.posts) && data.posts.length > 0 && (
-          <Featured item={data.posts[1]} key={data.posts[1]._id} />
+          <Featured item={data.posts[0]} key={data.posts[0]._id} />
         )}
         {!isValidating && (!data || data.posts.length === 0) && (
           <p>No posts available.</p>
