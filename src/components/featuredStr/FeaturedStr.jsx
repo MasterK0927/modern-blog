36ced8185c58e2f4a54f5123c72037dd8704f29b
MasterK0927 <btech10189.22@bitmesra.ac.in>
2024-09-24 04:40:39 +0530

    fixed the url

--- src/components/featuredStr/FeaturedStr.jsx
+++ src/components/featuredStr/FeaturedStr.jsx
@@ -23,7 +23,7 @@ const FeaturedStr = ({ cat }) => {
   const [page, setPage] = React.useState(1);
 
   const { data, error, isValidating } = useSWR(
-    `http://localhost:3001/api/posts?page=${page}&cat=${cat || ''}`,
+    `https://keshavwrites.netlify.app/api/posts?page=${page}&cat=${cat || ''}`,
     fetcher,
   );
 
